19a7b935e75784dbbabee43098bc355795a21190
nayon117 <hasibul.nayon1@gmail.com>
2024-03-14 15:12:22 +0600

    create some more news

--- src/components/ui/LatestNews/LatestNews.js
+++ src/components/ui/LatestNews/LatestNews.js
@@ -1,5 +1,14 @@
-import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
-import topNews from '@/assets/top-news.png'
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+import topNews from "@/assets/top-news.png";
+import topNews2 from "@/assets/top-news2.png";
 import Image from "next/image";
 
 const LatestNews = () => {
@@ -8,10 +17,12 @@ const LatestNews = () => {
       <Card className="my-6">
         <CardActionArea>
           <CardMedia>
-            <Image src={topNews} width={800} alt="top news image"/>
+            <Image src={topNews} width={800} alt="top news image" />
           </CardMedia>
           <CardContent>
-            <p className="bg-red-500 text-white w-28 px-2 py-1 rounded my-5">Technology</p>
+            <p className="bg-red-500 text-white w-28 px-2 py-1 rounded my-5">
+              Technology
+            </p>
             <Typography gutterBottom variant="h5" component="div">
               Bitcoin climbs as Elon Musk says Tesla will accept it again
             </Typography>
@@ -19,12 +30,116 @@ const LatestNews = () => {
               By Hasibul Hasan - March 14,2024
             </Typography>
             <Typography variant="body2" color="text.secondary">
-                Bitcoin climbed above $40,000 on Monday, after Elon Musk said Tesla would accept it as payment when miners use more clean energy.
+              Bitcoin climbed above $40,000 on Monday, after Elon Musk said
+              Tesla would accept it as payment when miners use more clean
+              energy.
             </Typography>
           </CardContent>
         </CardActionArea>
-        
       </Card>
+
+      <Grid className="mt-5" container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+        <Card className="my-6">
+        <CardActionArea>
+          <CardMedia>
+            <Image src={topNews2} width={800} alt="top news image" />
+          </CardMedia>
+          <CardContent>
+            <p className="bg-red-500 text-white w-28 px-2 py-1 rounded my-5">
+              Technology
+            </p>
+            <Typography gutterBottom >
+              Bitcoin climbs as Elon Musk says Tesla will accept it again
+            </Typography>
+            <Typography gutterBottom className="my-3">
+              By Hasibul Hasan - March 14,2024
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Bitcoin climbed above $40,000 on Monday, after Elon Musk said
+              Tesla would accept it as payment when miners use more clean
+              energy.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+        </Grid>
+        <Grid item xs={6}>
+        <Card className="my-6">
+        <CardActionArea>
+          <CardMedia>
+            <Image src={topNews2} width={800} alt="top news image" />
+          </CardMedia>
+          <CardContent>
+            <p className="bg-red-500 text-white w-28 px-2 py-1 rounded my-5">
+              Technology
+            </p>
+            <Typography gutterBottom >
+              Bitcoin climbs as Elon Musk says Tesla will accept it again
+            </Typography>
+            <Typography gutterBottom className="my-3">
+              By Hasibul Hasan - March 14,2024
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Bitcoin climbed above $40,000 on Monday, after Elon Musk said
+              Tesla would accept it as payment when miners use more clean
+              energy.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+        </Grid>
+        <Grid item xs={6}>
+        <Card className="my-6">
+        <CardActionArea>
+          <CardMedia>
+            <Image src={topNews2} width={800} alt="top news image" />
+          </CardMedia>
+          <CardContent>
+            <p className="bg-red-500 text-white w-28 px-2 py-1 rounded my-5">
+              Technology
+            </p>
+            <Typography gutterBottom >
+              Bitcoin climbs as Elon Musk says Tesla will accept it again
+            </Typography>
+            <Typography gutterBottom className="my-3">
+              By Hasibul Hasan - March 14,2024
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Bitcoin climbed above $40,000 on Monday, after Elon Musk said
+              Tesla would accept it as payment when miners use more clean
+              energy.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+        </Grid>
+        <Grid item xs={6}>
+        <Card className="my-6">
+        <CardActionArea>
+          <CardMedia>
+            <Image src={topNews2} width={800} alt="top news image" />
+          </CardMedia>
+          <CardContent>
+            <p className="bg-red-500 text-white w-28 px-2 py-1 rounded my-5">
+              Technology
+            </p>
+            <Typography gutterBottom >
+              Bitcoin climbs as Elon Musk says Tesla will accept it again
+            </Typography>
+            <Typography gutterBottom className="my-3">
+              By Hasibul Hasan - March 14,2024
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Bitcoin climbed above $40,000 on Monday, after Elon Musk said
+              Tesla would accept it as payment when miners use more clean
+              energy.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
