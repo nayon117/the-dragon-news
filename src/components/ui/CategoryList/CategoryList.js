0bd53f160ba377b020c8dc9177371510f4e0e688
nayon117 <hasibul.nayon1@gmail.com>
2024-03-15 07:33:07 +0600

    make category dynamic

--- src/components/ui/CategoryList/CategoryList.js
+++ src/components/ui/CategoryList/CategoryList.js
@@ -1,22 +1,25 @@
-
-
 import { getAllCategories } from "@/utils/getAllCategories";
 import { Box, Button, Divider, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 // import { useEffect, useState } from "react";
 
 const CategoryList = async () => {
-     const {data:allCategories} =await  getAllCategories()
-    //  console.log(allCategories);
-    return(
-         <Box className="mt-5 bg-gray-100 px-5 py-2" >
-            <Typography variant="h6" textAlign="center" >Categories</Typography>
-            <Divider/>
-            <Stack rowGap={1} sx={{mt:2.5}}>
-                {allCategories?.map(category=>(
-                    <Button variant="outlined" key={category.id}>{category.title}</Button>
-                ))}
-            </Stack>
-         </Box>
-    )
-}
+  const { data: allCategories } = await getAllCategories();
+  //  console.log(allCategories);
+  return (
+    <Box className="mt-5 bg-gray-100 px-5 py-2">
+      <Typography variant="h6" textAlign="center">
+        Categories
+      </Typography>
+      <Divider />
+      <Stack rowGap={1} sx={{ mt: 2.5 }}>
+        {allCategories?.map((category) => (
+          <Button variant="outlined" key={category.id}>
+          <Link href={category.title}>  {category.title}</Link>
+          </Button>
+        ))}
+      </Stack>
+    </Box>
+  );
+};
 export default CategoryList;
